16bef28162af5834444e1126242b19df8243e818
szuturon <ronnyszutu@gmail.com>
2022-05-05 22:39:36 -0400

    Accordion with Mounting and Unmounting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,21 @@
 <script>
+    import Accordion from '$lib/Accordion.svelte'
     import Toggle from '$lib/Toggle.svelte'
     let isToggled = false;
 </script>
 
 <h1>Welcome to Level UI</h1>
+
+<Accordion isOpen={true} buttonText="Do I need a credit card?">
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente aliquam, corrupti tenetur assumenda iure facilis accusantium iste aliquid vitae odio quaerat ducimus et vel animi explicabo amet debitis, ratione fugiat.
+    </p>
+</Accordion>
+<Accordion isOpen={false} buttonText="Do I need a credit card?">
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente aliquam, corrupti tenetur assumenda iure facilis accusantium iste aliquid vitae odio quaerat ducimus et vel animi explicabo amet debitis, ratione fugiat.
+    </p>
+</Accordion>
 
 <Toggle bind:isToggled label="Beta" />
 <Toggle bind:isToggled label="Beta" style="--toggleBackgroundColor: red;" />

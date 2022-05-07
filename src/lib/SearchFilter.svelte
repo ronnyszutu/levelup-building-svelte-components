bf82df2b3667cd99e1b316caafa733f66c931204
szuturon <ronnyszutu@gmail.com>
2022-05-06 23:57:36 -0400

    Search Filter with Reactive Statements

---
+++ src/lib/SearchFilter.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let items;
+    let search = '';
+
+    $: filteredSearch = items.filter((item) => {
+        return search === '' || item.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    })
+</script>
+
+<label for="">
+    Search Drinks: <br />
+    <input bind:value={search} type="text" placeholder="Search">
+</label>
+
+<ul>
+    {#each filteredSearch as item}
+        <li>{item}</li>
+    {/each}
+</ul>
+
+<style>
+    ul {
+        text-transform: capitalize;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,31 @@
 <script>
+	import SearchFilter from '$lib/SearchFilter.svelte';
     import BetterAccordion from '$lib/BetterAccordion.svelte'
     import Toggle from '$lib/Toggle.svelte'
     let isToggled = false;
+    let items = [
+        "coffee",
+        "tea",
+        "pepsi",
+        "bbt",
+        "sprite",
+        "juice",
+        "Coke",
+    ];
 </script>
 
 <h1>Welcome to Level UI</h1>
 
-<BetterAccordion isOpen={true} buttonText="Do I need a credit card?">
+<SearchFilter {items} />
+
+<!-- <BetterAccordion isOpen={true} buttonText="Do I need a credit card?">
     <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente aliquam, corrupti tenetur assumenda iure facilis accusantium iste aliquid vitae odio quaerat ducimus et vel animi explicabo amet debitis, ratione fugiat.
     </p>
-</BetterAccordion>
+</BetterAccordion> -->
 
-<Toggle bind:isToggled label="Beta" />
-<Toggle bind:isToggled label="Beta" style="--toggleBackgroundColor: red;" />
+<!-- <Toggle bind:isToggled label="Beta" />
+<Toggle bind:isToggled label="Beta" style="--toggleBackgroundColor: red;" /> -->
 
 {#if isToggled}
     <h1>I'm toggled</h1>
